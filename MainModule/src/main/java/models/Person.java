7c236e40466a8259d56e9cc495591d30edd3f0d3
git-user-cpp <akushyk799@gmail.com>
2023-05-10 08:58:41 +0300

    In file Person.java - regular comments were replaced by JavaDoc - in constructor was implemented lazy initialization - output implemented via using iterator

--- MainModule/src/main/java/models/Person.java
+++ MainModule/src/main/java/models/Person.java
@@ -5,24 +5,42 @@ import java.util.ArrayList;
 
 public class Person {
 
-    //list of Strings for holding data
-    private List<String> fields = new ArrayList<String>();
+    /**
+     * A list of Strings to hold the person's information.
+     */
+    private List<String> fields;
 
-    //Constructor for adding fields into the list
+    /**
+     * fields
+     */
+    String id, name, year;
+
+    /**
+     * Constructs a Person object with the given id, name, and year information.
+     * @param id the person's id
+     * @param name the person's name
+     * @param year the year the person was born
+     */
     public Person(String id, String name, String year){
-        fields.add(id);
-        fields.add(name);
-        fields.add(year);
+        this.fields = null;
+        this.id = id;
+        this.name = name;
+        this.year = year;
     }
 
-    //output function
-    public void printPerson(){
-
-        int i = 0;
-        //loop for outputting info
-        while(i < fields.size()){
-            System.out.println(fields.get(i));
-            i++;
+    /**
+     * Prints the person's information.
+     */
+    public void printPerson() {
+        if (this.fields == null) {
+            this.fields = new ArrayList<String>();
+            this.fields.add(this.id);
+            this.fields.add(this.name);
+            this.fields.add(this.year);
+        }
+        for (String field : this.fields) {
+            System.out.println(field);
         }
     }
 }
+
