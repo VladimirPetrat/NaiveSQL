1d5774630cb58664cb9f8adbf6a3bcb5d2cc45c0
git-user-cpp <akushyk799@gmail.com>
2023-05-16 14:12:19 +0300

    file StartClass.java was cleaned

--- MainModule/src/main/java/StartClass.java
+++ MainModule/src/main/java/StartClass.java
@@ -1,24 +1,7 @@
-import models.PersonDTO;
-
 public class StartClass {
 
     public static void main(String... args) {
 
-        PersonDTO me = new PersonDTO("1", "Andy", "2001");
 
-        System.out.println(me.returnPersonDTO());
-
-        me.replaceField("id", "2");
-        me.replaceField("name", "Andrew");
-        me.replaceField("year", "1999");
-        System.out.println(me.returnPersonDTO());
-
-        me.removeField("year");
-        System.out.println(System.lineSeparator() + "ID = " + me.returnField("id") +
-                            System.lineSeparator() + "Name = " + me.returnField("name")+
-                            System.lineSeparator() + "Year = " + me.returnField("year"));
-
-        me.addField("second_name", "Kushyk");
-        System.out.println(me.returnPersonDTO());
     }
 }
